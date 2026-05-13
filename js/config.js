9dc22f82ca828d6b3aabb7e99a1a0d3ea67751b1
maitrilearn <maitrilearn@gmail.com>
2026-05-13 12:15:13 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,5 +2,5 @@ const SUPABASE_URL = "https://rairwoyaesgvezxyztnq.supabase.co";
 
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJhaXJ3b3lhZXNndmV6eHl6dG5xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE1NTIwODcsImV4cCI6MjA3NzEyODA4N30.1TxIpecWR2YoP_6wz-Ifs3VWWfuhLND5ob3pLYzJM_g";
 
-const BACKEND_URL = "http://localhost:5000";
+const BACKEND_URL = "https://maitrilearn-backend-130526.onrender.com";
 
